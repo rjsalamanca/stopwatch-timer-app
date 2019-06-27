b20615438fd090fb805bc1b900782a2e09c1704a
RJ Salamanca <salamancaronito@gmail.com>
2019-06-27 14:15:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const stopwatchStyles = {
+  border:'1px solid red',
+  width:'200px',
+  margin:'50px auto 0'
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="stopwatch" style={stopwatchStyles}>
+          <div>Timer: <span>0</span></div>
+          <button>START</button>
+          <button>STOP</button>
+          <button>RESET</button>
+        </div>
+        <div className="timer">
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
